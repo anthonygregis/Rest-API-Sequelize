171cf38e01dac61a2c526a0ab2959bc8687237f1
thedevtron <anthony@thedevtron.com>
2020-03-08 23:17:48 -0400

    Initial API Commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "nice-dude-still-need-to-meta-game-when-you-arent-even-in-the-server"
+};
---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "149.28.58.181",
+  USER: "adminapi",
+  PASSWORD: "0ne5H9k*",
+  DB: "dev_api",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
