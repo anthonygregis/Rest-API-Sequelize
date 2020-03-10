817980a0f07bee8b0ff7a6f0742b45a675b725d5
thedevtron <anthony@thedevtron.com>
2020-03-10 19:33:06 -0400

    CORS location

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3001"
+  origin: "http://api.nopixel.online/"
 };
 
 app.use(cors(corsOptions));
